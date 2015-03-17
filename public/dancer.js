363dd0e38ddb51ca535d016a3ecab3850265f250
Jack Desert <jackdesert@gmail.com>
2015-03-17 09:00:18 -0700

    Now allows text selection within a message, and also toggles visibility on a fast click

--- public/dancer.js
+++ public/dancer.js
@@ -1,13 +1,31 @@
+
 $(document).ready(function(){
-  $('.message').click(function(event){
-    console.log(event.target.nodeName)
+
+  toggleMessageOnFastClick()
+
+})
 
 
-    if (event.target.nodeName !== 'A'){
-      // Only toggle if they are NOT clicking on a link
+var toggleMessageOnFastClick = function(){
+
+  var fastClickTime = 200
+  var lastMouseDown = Date.now()
+
+  $('.message').mousedown(function(event){
+    lastMouseDown = Date.now()
+  })
+
+  $('.message').mouseup(function(event){
+    var elapsedTime = Date.now() - lastMouseDown
+
+    // console.log(event.target.nodeName)
+
+    // Only toggle if they are NOT clicking on a link
+    // AND if the mousedown was very recent (indicating no text selection)
+    if (event.target.nodeName !== 'A' && elapsedTime < fastClickTime ){
       $(this).children('.details').toggleClass('hidden')
     }
 
   })
-})
+}
 

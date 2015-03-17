be971ef7b3941abd57e5e4cedb4332da5ffdaf33
Jack Desert <jackdesert@gmail.com>
2015-03-17 09:13:16 -0700

    Now using absolute value in distance determination

--- public/dancer.js
+++ public/dancer.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 var toggleMessageOnFastClick = function(){
 
 
-  var resolution = 4
+  var squaredResolution = 16
   var lastX = 0
   var lastY = 0
 
@@ -21,13 +21,13 @@ var toggleMessageOnFastClick = function(){
   $('.message').mouseup(function(event){
     var deltaX = event.pageX - lastX
     var deltaY = event.pageY - lastY
-    var deltaTotal = deltaX + deltaY
+    var deltaSquared = deltaX * deltaX + deltaY * deltaY
 
     // console.log(event.target.nodeName)
 
     // Only toggle if they are NOT clicking on a link
     // AND if the mousedown was very recent (indicating no text selection)
-    if (event.target.nodeName !== 'A' && deltaTotal < resolution ){
+    if (event.target.nodeName !== 'A' && deltaSquared < squaredResolution ){
       $(this).children('.details').toggleClass('hidden')
     }
 

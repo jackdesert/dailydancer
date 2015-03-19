38522d4b66a5b3e8c4be270e047812234bebf515
Jack Desert <jackdesert@gmail.com>
2015-03-19 04:36:19 +0000

    ApplicationHelper has better filter for forwarded email note

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # Note the FOOTER_REGEX has the /m flag which allows it to match multiple lines
   FOOTER_REGEX = /-------------------------------------------------------------------.*/m
 
-  YAHOO_FROM_REGEX = /This email was sent from yahoo.com which does not allow forwarding of emails via email lists. Therefore the sender's email address \(.*\) has been replaced with a dummy one. The original message follows:/
+  FORWARDED_EMAIL_REGEX = /This email was sent from \w{1,25}\.com which does not allow forwarding of emails via email lists. Therefore the sender's email address \(.*\) has been replaced with a dummy one. The original message follows:/
 
     SUBJECT_SNIP = '[SacredCircleDance] '
 
@@ -20,7 +20,7 @@ module ApplicationHelper
     raise ArgumentError, 'Expected a Message' unless message.is_a?(Message)
     text = message.plain
     text.sub!(FOOTER_REGEX, '')
-    text.sub!(YAHOO_FROM_REGEX, '')
+    text.sub!(FORWARDED_EMAIL_REGEX, '')
     text = insert_hyperlinks(text)
     text.split(LINE_FEED)
   end

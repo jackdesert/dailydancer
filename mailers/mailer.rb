da26cbcdd6b832bbcd06bbd93edec184bd88ea1e
Jack Desert <jackdesert@gmail.com>
2015-05-01 03:00:08 +0000

    placeholder--Its RUNNING

--- mailers/mailer.rb
+++ mailers/mailer.rb
@@ -25,11 +25,12 @@ class Mailer
 
       subject = "Your Event Has Been Listed on Daily Dancer"
 
-      pretty_date = message.parsed_date.strftime('%b %d')
-      pretty_date_with_weekday = message.parsed_date.strftime('%A, %b %d')
+      date_object = message.parsed_date.to_date
+      pretty_date = date_object.strftime('%b %d')
+      pretty_date_with_weekday = date_object.strftime('%A, %b %d')
       author_first_name = message.author.split('<').first
 
-      body  = haml :'mailers/confirm_listing', locals: { message: message }
+      body  = erb :'mailers/confirm_listing', locals: { message: message }
       build_email(to, subject, body)
     end
 
--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe Mailer do
 
   let(:author) { 'Jack <jackdesert@gmail.com>' }
-  let(:message) { create(:message, author: author) }
+  let(:message) { create(:message, author: author, plain: 'March 2') }
   describe '.confirm_listing' do
     it do
-      described_class.confirm_listing(message)
+      email = described_class.confirm_listing(message)
+      email.should_not be_nil
     end
   end
 end
